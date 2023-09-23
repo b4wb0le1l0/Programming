cd28b8f8f0e25664f39f87d84fbc12009c42a8b5
b4wb0le1l0 <91735768+b4wb0le1l0@users.noreply.github.com>
2023-09-23 12:01:11 +0300

    Delete data directory

--- data/src/main/kotlin/utility/Request.kt
+++
@@ -1,9 +0,0 @@
-package utility
-
-import java.io.Serializable
-
-data class Request(
-    val commandName: String,
-    val commandArguments: String,
-    val serverToken: String? = null
-) : Serializable
